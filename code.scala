92b0e2cbbe461809f328893d8cb33df929e30515
deepaksyadav <deepaksyadav@users.noreply.github.com>
2023-02-24 00:26:18 +0530

    Update code.scala

--- code.scala
+++ code.scala
@@ -29,3 +29,25 @@ class Person(var firstName: String, var lastName: String):
   println("initialization ends")
 val john = Person("John", "Doe")
 john.printFullName
+
+// 24-Feb-2023
+// case class demo
+//Case Class, Immutable 
+
+case class Rectangle(width: Int, height: Int):
+	val area = width * height
+
+val facade = Rectangle(5,3)
+println(facade.area)
+
+
+// facade.height=10 - ERROR, updating val
+
+val smallRectangle = Rectangle (width =2, height =4)
+val largeRectangle = smallRectangle.copy(width =4)
+
+println(smallRectangle.area)
+
+println(largeRectangle.area)
+
+
